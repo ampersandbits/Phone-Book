e96599bae99413883cd94ce446b4a670fc07ae31
karthik <nagakarthik143@gmail.com>
2017-12-29 17:10:18 -0500

    Addedstyle to the page

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
+var contacts = [
+    {firstname:"abc", lastname:"qwe", number:4437605137, city:"Crofton"},
+    {firstname:" dinesh",lastname:"namburi", number:8546584412, city:"new jersey"},
+    {firstname:"Dheeraj",lastname:"Batchu", number:6549876652, city:"Atlanta" }
+    ];
 
 app.get("/", function(req,res){
-    var contacts = [
-        {name:"abc", number:4437605137, city:"Crofton"},
-        {name:" dinesh", number:8546584412, city:"new jersey"},
-        {name:"Dheeraj", number:6549876652, city:"Atlanta" }
-        ];
     res.render("contacts",{contacts:contacts});
+    //res.json(contacts);
 });
 
 app.get("/new", function(req, res) {
@@ -18,13 +21,20 @@ app.get("/new", function(req, res) {
 });
 
 app.post("/",function(req,res){
-    var name = req.body.name ;
-    var image = req.body.number;
+    //console.log(req.body);
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var number = req.body.number;
     var city = req.body.city;
-    var newContact = {name : name, image : image, city:city};
+    var newContact = {firstname : firstname, lastname : lastname, number : number, city : city};
     contacts.push(newContact);
     res.redirect("/");
+   // res.send("ok");
 });
+
+app.get("/showcontact", function(req,res){
+     res.render("showContact",{showcontact:contacts});
+ });
 
 app.get("*", function(req,res){
     res.send("Page Error");
