8f98548d8643625902742a2289e18f4d81978638
karthik <nagakarthik143@gmail.com>
2018-01-03 14:49:44 -0500

    added new route, created database, generated an id for each contact so that we can have a look of each contact

--- app.js
+++ app.js
@@ -3,17 +3,47 @@ var bodyParser = require("body-parser");
 var app = express();
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/contacts");
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-var contacts = [
-    {firstname:"abc", lastname:"qwe", number:4437605137, city:"Crofton", index:1},
-    {firstname:"dinesh",lastname:"namburi", number:8546584412, city:"New jersey", index:2},
-    {firstname:"Dheeraj",lastname:"Batchu", number:6549876652, city:"Atlanta", index:3 }
-    ];
-// index
+var contactSchema = new mongoose.Schema({
+    firstname : String,
+    lastname : String,
+    number : Number,
+    city : String
+});
+
+var Contact = mongoose.model("Contact", contactSchema);
+// Contact.create(
+//     {
+//         firstname : "Karthik",
+//         lastname : "Nani",
+//         number : 4437605137,
+//         city : "Crofton"
+//     }, function(err,contact){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("newly created");
+//             console.log(contact);
+//         }
+//     }
+// )
+// var contacts = [
+//     {firstname:"abc", lastname:"qwe", number:4437605137, city:"Crofton", id:1},
+//     {firstname:"dinesh",lastname:"namburi", number:8546584412, city:"New jersey", id:2},
+//     {firstname:"Dheeraj",lastname:"Batchu", number:6549876652, city:"Atlanta", id:3 }
+//     ];
+//index
 app.get("/", function(req,res){
-    res.render("contacts",{contacts:contacts});
+    Contact.find({}, function(err, allcontacts){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("contacts",{contacts:allcontacts});
+        }
+    });
     //res.json(contacts);
 });
 // new
@@ -28,21 +58,25 @@ app.post("/",function(req,res){
     var number = req.body.number;
     var city = req.body.city;
     var newContact = {firstname : firstname, lastname : lastname, number : number, city : city};
-    contacts.push(newContact);
-    res.redirect("/");
+    Contact.create(newContact, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/");
+        }
+    })
    // res.send("ok");
 });
 // show
-app.get("/contact/:index", function(req,res){
-    contacts.findIndex(req.params.id, function(err, foundContact){
+app.get("/contact/:id", function(req,res){
+    Contact.findById(req.params.id, function(err, foundContact){
         if(err){
-            console.log("err");
-        }
-        else{
-            res.render("showContact",{showcontact:foundContact}); 
+            console.log(err);
+        } else {
+            res.render("show", {contact:foundContact});
         }
     });
- });
+});
 
 app.get("*", function(req,res){
     res.send("Page Error");
