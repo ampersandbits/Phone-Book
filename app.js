b61eae46bfd7c08a181399eb57e95d696583e2c9
karthik <nagakarthik143@gmail.com>
2018-01-02 23:16:15 -0500

    given id to each contact

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 var contacts = [
-    {firstname:"abc", lastname:"qwe", number:4437605137, city:"Crofton"},
-    {firstname:"dinesh",lastname:"namburi", number:8546584412, city:"New jersey"},
-    {firstname:"Dheeraj",lastname:"Batchu", number:6549876652, city:"Atlanta" }
+    {firstname:"abc", lastname:"qwe", number:4437605137, city:"Crofton", index:1},
+    {firstname:"dinesh",lastname:"namburi", number:8546584412, city:"New jersey", index:2},
+    {firstname:"Dheeraj",lastname:"Batchu", number:6549876652, city:"Atlanta", index:3 }
     ];
-
+// index
 app.get("/", function(req,res){
     res.render("contacts",{contacts:contacts});
     //res.json(contacts);
 });
-
+// new
 app.get("/new", function(req, res) {
     res.render("new");
 });
-
+// create
 app.post("/",function(req,res){
     //console.log(req.body);
     var firstname = req.body.firstname;
@@ -31,9 +32,16 @@ app.post("/",function(req,res){
     res.redirect("/");
    // res.send("ok");
 });
-
-app.get("/showcontact", function(req,res){
-     res.render("showContact",{showcontact:contacts});
+// show
+app.get("/contact/:index", function(req,res){
+    contacts.findIndex(req.params.id, function(err, foundContact){
+        if(err){
+            console.log("err");
+        }
+        else{
+            res.render("showContact",{showcontact:foundContact}); 
+        }
+    });
  });
 
 app.get("*", function(req,res){
